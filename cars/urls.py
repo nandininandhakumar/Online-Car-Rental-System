bf998d9dcb8715466836308f00b2ec0b32eb2c1f
Nandini Nandhakumar <mailtonandininandus@gmail.com>
2022-04-25 11:11:46 +0530

    converted demo project to class based view

--- cars/urls.py
+++ cars/urls.py
@@ -1,4 +1,5 @@
-from atexit import register
+
+from re import template
 from django.urls import path
 
 from .views import *
@@ -6,12 +7,12 @@ from .views import *
 urlpatterns = [
     path('',Index.as_view(),name="index" ),
     path('registraion/',Registration.as_view(),name="cars/registration/registration"),
-    path('login/',login,name="cars/registration/login"),
-    path('customer_index/',customer_index,name="cars/customer/customer_index"),
-    path('owner_index/',owner_index,name="cars/carshop_owner/owner_index"),
-    path('add_car/',add_car,name="cars/carshop_owner/add_car"),
-    path('edit_car/<int:id>',edit_car,name="cars/cashop_owner/edit_car"),
-    path('delete_car/<int:id>',delete_car,name="cars/cashop_owner/delete_car"),
-    path('book_car/',book_car,name="cars/customer/book_car"),
-    path('logout/',logout,name="logout"),
+    path('login/',Login.as_view(),name="cars/registration/login"),
+    path('customer_index/',Customer_index.as_view(),name="cars/customer/customer_index"),
+    path('owner_index/',Owner_index.as_view(),name="cars/carshop_owner/owner_index"),
+    path('add_car/',Add_car.as_view(),name="cars/carshop_owner/add_car"),
+    path('edit_car/<int:id>/',Edit_car.as_view(),name="cars/cashop_owner/edit_car"),
+    path('delete_car/<int:id>',Delete_car.as_view(),name="cars/cashop_owner/delete_car"),
+    path('book_car/',Book_car.as_view(),name="cars/customer/book_car"),
+    path('logout/',Logout.as_view(),name="logout"),
 ]
